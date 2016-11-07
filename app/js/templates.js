72de0aeacf698d98c3e5d671fbd855ae2a8ef924
wightman <rick@wightmanfamily.org>
2016-11-07 19:12:36 -0400

    image grid using flexbox added

--- app/js/templates.js
+++ app/js/templates.js
@@ -1,4 +1,4 @@
 angular.module('templates', []).run(['$templateCache', function($templateCache) {$templateCache.put('details.html','');
-$templateCache.put('home.html','<div class="image-grid">\r\n    <div ng-repeat="photo in home.photos">\r\n        <img class="tile" src="{{photo.images[0].url}}"/>\r\n    </div>\r\n</div>');
+$templateCache.put('home.html','<div class="image-grid">\n    <div ng-repeat="photo in home.photos">\n        <img class="tile" src="{{photo.images[0].url}}"/>\n    </div>\n</div>\n');
 $templateCache.put('search.html','');
-$templateCache.put('directives/example.html','<div class="example-directive">\r\n  <h1>Directive title: {{title}}</h1>\r\n  <p>This is an example of a directive, click me!</p>\r\n</div>\r\n');}]);
+$templateCache.put('directives/example.html','<div class="example-directive">\n  <h1>Directive title: {{title}}</h1>\n  <p>This is an example of a directive, click me!</p>\n</div>\n');}]);
